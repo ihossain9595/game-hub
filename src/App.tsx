fa047e06e0547813c9b9082ba5b4a226596cb288
ihossain9595 <ismail.9595@gmail.com>
2023-05-07 22:02:04 +0600

    check

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ import GenreList from "./components/GenreList";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 
+interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+}
+
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
   const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
